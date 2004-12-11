b064e40075276152b18f4a3e145256ff43873f1d
Vinay Kumar <vinay.kumar@nih.gov>
2004-12-11 17:07:48 +0000

    *** empty log message ***
    SVN-Revision: 175

---
+++ api/src/gov/nih/nci/security/util/ObjectSetUtil.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Dec 11, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.security.util;
+
+import java.util.*;
+
+/**
+ * 
+ * 
+ * @author Vinay Kumar (Ekagra Software Technologies Ltd.)
+ */
+public class ObjectSetUtil {
+	
+	public static Collection intersect(Collection firstSet,Collection secondSet){
+		return null;
+	}
+	public static Collection minus(Collection toBeDeletedFrom,Collection tobeDeleted){
+		return null;
+	}
+    public static Collection union(Collection firstSet,Collection secondSet){
+    	return null;
+    }
+}
