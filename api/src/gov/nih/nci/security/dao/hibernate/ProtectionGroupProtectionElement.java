8db661bc9106146444331cf5316bac4949255e59
Vinay Kumar <vinay.kumar@nih.gov>
2004-12-15 18:55:49 +0000

    *** empty log message ***
    SVN-Revision: 221

---
+++ api/src/gov/nih/nci/security/dao/hibernate/ProtectionGroupProtectionElement.java
@@ -0,0 +1,124 @@
+/*
+ * Created on Dec 15, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.security.dao.hibernate;
+
+import java.io.Serializable;
+import java.util.Date;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/** 
+ *        @hibernate.class
+ *         table="protection_group_protection_element"
+ *     
+*/
+public class ProtectionGroupProtectionElement implements Serializable {
+
+    /** identifier field */
+    private Long protectionGroupProtectionElementId;
+
+    /** persistent field */
+    private Date updateDate;
+
+    /** persistent field */
+    private gov.nih.nci.security.authorization.domainobjects.ProtectionElement protectionElement;
+
+    /** persistent field */
+    private gov.nih.nci.security.authorization.domainobjects.ProtectionGroup protectionGroup;
+
+    /** full constructor */
+    public ProtectionGroupProtectionElement(Long protectionGroupProtectionElementId, Date updateDate, gov.nih.nci.security.authorization.domainobjects.ProtectionElement protectionElement, gov.nih.nci.security.authorization.domainobjects.ProtectionGroup protectionGroup) {
+        this.protectionGroupProtectionElementId = protectionGroupProtectionElementId;
+        this.updateDate = updateDate;
+        this.protectionElement = protectionElement;
+        this.protectionGroup = protectionGroup;
+    }
+
+    /** default constructor */
+    public ProtectionGroupProtectionElement() {
+    }
+
+    /** 
+     *            @hibernate.id
+     *             generator-class="assigned"
+     *             type="java.lang.Long"
+     *             column="PROTECTION_GROUP_PROTECTION_ELEMENT_ID"
+     *         
+     */
+    public Long getProtectionGroupProtectionElementId() {
+        return this.protectionGroupProtectionElementId;
+    }
+
+    public void setProtectionGroupProtectionElementId(Long protectionGroupProtectionElementId) {
+        this.protectionGroupProtectionElementId = protectionGroupProtectionElementId;
+    }
+
+    /** 
+     *            @hibernate.property
+     *             column="UPDATE_DATE"
+     *             length="10"
+     *             not-null="true"
+     *         
+     */
+    public Date getUpdateDate() {
+        return this.updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    /** 
+     *            @hibernate.many-to-one
+     *             not-null="true"
+     *            @hibernate.column name="PROTECTION_ELEMENT_ID"         
+     *         
+     */
+    public gov.nih.nci.security.authorization.domainobjects.ProtectionElement getProtectionElement() {
+        return this.protectionElement;
+    }
+
+    public void setProtectionElement(gov.nih.nci.security.authorization.domainobjects.ProtectionElement protectionElement) {
+        this.protectionElement = protectionElement;
+    }
+
+    /** 
+     *            @hibernate.many-to-one
+     *             not-null="true"
+     *            @hibernate.column name="PROTECTION_GROUP_ID"         
+     *         
+     */
+    public gov.nih.nci.security.authorization.domainobjects.ProtectionGroup getProtectionGroup() {
+        return this.protectionGroup;
+    }
+
+    public void setProtectionGroup(gov.nih.nci.security.authorization.domainobjects.ProtectionGroup protectionGroup) {
+        this.protectionGroup = protectionGroup;
+    }
+
+    public String toString() {
+        return new ToStringBuilder(this)
+            .append("protectionGroupProtectionElementId", getProtectionGroupProtectionElementId())
+            .toString();
+    }
+
+    public boolean equals(Object other) {
+        if ( !(other instanceof ProtectionGroupProtectionElement) ) return false;
+        ProtectionGroupProtectionElement castOther = (ProtectionGroupProtectionElement) other;
+        return new EqualsBuilder()
+            .append(this.getProtectionGroupProtectionElementId(), castOther.getProtectionGroupProtectionElementId())
+            .isEquals();
+    }
+
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .append(getProtectionGroupProtectionElementId())
+            .toHashCode();
+    }
+
+}
