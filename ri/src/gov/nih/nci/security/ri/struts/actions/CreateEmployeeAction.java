0400e04601e764f3b56d685f6e8e6f5382a14459
hustedb <hustedb>
2005-01-07 21:21:43 +0000

    initial release
    SVN-Revision: 303

---
+++ ri/src/gov/nih/nci/security/ri/struts/actions/CreateEmployeeAction.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jan 7, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.security.ri.struts.actions;
+
+import org.apache.struts.action.Action;
+
+/**
+ * @author Brian
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CreateEmployeeAction extends Action {
+
+}
