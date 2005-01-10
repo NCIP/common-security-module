2ed651b5869d82efd42d21b2f8cbb19fc733d089
hustedb <hustedb>
2005-01-10 23:32:24 +0000

    initial release
    SVN-Revision: 305

---
+++ ri/src/gov/nih/nci/security/ri/struts/actions/LoginAction.java
@@ -0,0 +1,29 @@
+
+package gov.nih.nci.security.ri.struts.actions;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+/**
+ * Logs a User into the RI using the CSM Authentication module.
+ * @author Brian Husted
+ *
+*/
+public class LoginAction extends Action {
+	
+	
+
+	/* (non-Javadoc)
+	 * @see org.apache.struts.action.Action#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute(mapping, form, request, response);
+	}
+}
