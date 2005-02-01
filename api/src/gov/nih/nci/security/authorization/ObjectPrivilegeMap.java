f0675c852eb2414211440ffd4f2edf96edc94461
Vinay Kumar <vinay.kumar@nih.gov>
2005-02-01 22:45:54 +0000

    *** empty log message ***
    SVN-Revision: 478

---
+++ api/src/gov/nih/nci/security/authorization/ObjectPrivilegeMap.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Feb 1, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.security.authorization;
+
+/**
+ * @author kumarvi
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ObjectPrivilegeMap {
+
+}
