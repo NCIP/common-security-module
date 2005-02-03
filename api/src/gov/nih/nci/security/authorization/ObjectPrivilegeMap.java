0bf2c434a1adf543bbc2406340bb1fe4078d4e2d
Vinay Kumar <vinay.kumar@nih.gov>
2005-02-03 18:23:33 +0000

    *** empty log message ***
    SVN-Revision: 505

--- api/src/gov/nih/nci/security/authorization/ObjectPrivilegeMap.java
+++ api/src/gov/nih/nci/security/authorization/ObjectPrivilegeMap.java
@@ -1,8 +1,5 @@
 /*
  * Created on Feb 1, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 package gov.nih.nci.security.authorization;
 
@@ -11,23 +8,31 @@ import java.util.Collection;
 import gov.nih.nci.security.authorization.domainobjects.ProtectionElement;
 
 /**
+ *  This class represents the privileges for a protection Element for a user.
  * @author kumarvi
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class ObjectPrivilegeMap {
 	
 	private ProtectionElement pe;
 	private Collection privileges;
 	
+	
 	public ObjectPrivilegeMap(ProtectionElement pe,Collection privileges){
 		this.pe=pe;
 		this.privileges= privileges;
 	}
+	
+    /**
+     * @return ProtectionElement
+     */
     public ProtectionElement getProtectionElement(){
     	return this.pe;
     }
+    
+    /**
+     * Return the collection of privileges for the protection element in the context.
+     * @return Collection
+     */
     public Collection getPrivileges(){
     	return privileges;
     }
