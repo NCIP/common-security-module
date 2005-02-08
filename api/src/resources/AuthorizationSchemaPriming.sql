24dceff9e14961a62765303d494b1cceeb17e0be
Vinay Kumar <vinay.kumar@nih.gov>
2005-02-08 16:36:55 +0000

    *** empty log message ***
    SVN-Revision: 531

---
+++ api/src/resources/AuthorizationSchemaPriming.sql
@@ -0,0 +1,2 @@
+insert into application(APPLICATION_NAME,APPLICATION_DESCRIPTION,DECLARATIVE_FLAG,ACTIVE_FLAG,UPDATE_DATE)
+values ("<<application_context_name>>","Application Description",0,1,sysdate());
