3a76a9cd70013832f537ef9d304ceda11689a650
hustedb <hustedb>
2005-03-14 02:04:13 +0000

    Interface for permissions
    SVN-Revision: 664

---
+++ ri/src/gov/nih/nci/security/ri/util/Permissions.java
@@ -0,0 +1,17 @@
+
+package gov.nih.nci.security.ri.util;
+
+/**
+ * @author Brian
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public interface Permissions {
+
+	public static final String READ = "READ";
+	public static final String UPDATE = "UPDATE";
+	public static final String DELETE = "DELETE";
+	public static final String CREATE = "CREATE";
+	
+}
