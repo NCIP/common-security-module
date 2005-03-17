d4560ae64dd2e7ca611f69e224a2bf2bb22018f5
Kunal Modi <modik@mail.nih.gov>
2005-03-17 18:23:23 +0000

    Added JavaDocs
    SVN-Revision: 700

--- api/src/gov/nih/nci/security/AuthorizationManager.java
+++ api/src/gov/nih/nci/security/AuthorizationManager.java
@@ -126,55 +126,64 @@ public interface AuthorizationManager {
 
 	
 	/**
-	 * The method checks the permission for a {@link Subject} for a given {@link AccessPermission}
-	 * @param permission
-	 * @param subject
+	 * The method checks the permission for a {@link Subject} for a given {@link AccessPermission}. The {@link Subject}
+	 * is nothing but the <code>JAAS</code> subject and the {@link AccessPermission} is collection of the 
+	 * resource on the operation is to be performed and the actual operation itself.
+	 * @param permission The collection of resource and the operation to be performed on it
+	 * @param subject The <code>JAAS</code> representation of the user.
 	 * 
-	 * @return boolean
-	 * @throws CSException
+	 * @return boolean Returns true if the user has permission to perform the operation on that particular resource
+	 * @throws CSException If there are any errors while checking for permission
 	 */
 	public boolean checkPermission(AccessPermission permission, Subject subject) throws CSException;
 
 	/**
-	 * The method checks the permission for a user for a given {@link AccessPermission}
+	 * The method checks the permission for a {@link User} for a given {@link AccessPermission}. The 
+	 * {@link AccessPermission} is collection of the resource on the operation is to be performed and 
+	 * the actual operation itself. The userName is used to to obtain the User object and then the check
+	 * permission operation is performed to see if the user has the required access or not.
+	 * @param permission The collection of resource and the operation to be performed on it
+	 * @param userName The user name of the user which is trying to perform the operation
 	 * 
-	 * @param permission
-	 * @param userName
-	 * 
-	 * @return boolean
-	 * @throws CSException
+	 * @return boolean Returns true if the user has permission to perform the operation on that particular resource
+	 * @throws CSException If there are any errors while checking for permission
 	 */
 	public boolean checkPermission(AccessPermission permission, String userName) throws CSException;
 
 	/**
-	 * The method checks if the user has passed privilege on the atribute for the passed object.
-	 * @param userName
-	 * @param objectId
-	 * @param attributeName
-	 * @param privilegeName
+	 * The method checks the permission for a {@link User} for a given {@link ProtectionElement}. The 
+	 * {@link ProtectionElement} is obtained using the object id and the attribute name both. 
+	 * The userName is used to to obtain the User object. Then the check
+	 * permission operation is performed to see if the user has the required access or not.
+	 * @param userName The user name of the user which is trying to perform the operation
+	 * @param objectId The object id of the protection element on which the user wants to perform the operation
+	 * @param attributeName The attribute of the protection element on which the user wants to perform the operation
+	 * @param privilegeName The operation which the use wants to perform on the protection element
 	 * 
-	 * @return boolean
-	 * @throws CSException
+	 * @return boolean Returns true if the user has permission to perform the operation on that particular resource
+	 * @throws CSException If there are any errors while checking for permission
 	 */
 	public boolean checkPermission(String userName, String objectId, String attributeName, String privilegeName) throws CSException;
 
 	/**
-	 * This method verifies whether the User has the provided Privilege on the Protection Element.
-	 * @param userName
-	 * @param objectId
-	 * @param privilegeName
+	 * The method checks the permission for a {@link User} for a given {@link ProtectionElement}. The 
+	 * {@link ProtectionElement} is obtained using the object id only. 
+	 * The userName is used to to obtain the User object. Then the check
+	 * permission operation is performed to see if the user has the required access or not.
+	 * @param userName The user name of the user which is trying to perform the operation
+	 * @param objectId The object id of the protection element on which the user wants to perform the operation
+	 * @param privilegeName The operation which the use wants to perform on the protection element
 	 * 
-	 * @return boolean
-	 * @throws CSTransactionException
+	 * @return boolean Returns true if the user has permission to perform the operation on that particular resource
+	 * @throws CSException If there are any errors while checking for permission
 	 */
 	public boolean checkPermission(String userName, String objectId, String privilegeName) throws CSException;
 
 	/**
-	 * The method returns all the principals for the user
-	 *  The method return 
-	 * @param userName
+	 * The method returns all the principals for the user.
+	 * @param userName The user name whose principals we are trying to obtain
 	 * 
-	 * @return Principal[]
+	 * @return Principal[] The <code>JAAS</code> Principals for the given user
 	 */
 	public Principal[] getPrincipals(String userName);
 
@@ -210,11 +219,14 @@ public interface AuthorizationManager {
 	public void assignProtectionElement(String protectionGroupName, String protectionElementObjectId)throws CSTransactionException;
 
 	/**
-	 * @param userName
-	 * @param protectionElementObjectId
-	 * @param protectionElementAttributeName
+	 * The methods sets the ownership of a given {@link ProtectionElement} to the given {@link User}. The protectio element
+	 * is obtained using the object id and attribute name passed and the user is retrieved using the user name passed
+	 * @param userName The user name of the user who is to become the owner of the protection element
+	 * @param protectionElementObjectId The object id of the protection element which is to be associated with the user
+	 * @param protectionElementAttributeName The attribute name of the protection element which is to be associated with the user
 	 * 
-	 * @throws CSTransactionException
+	 * @throws CSTransactionException If there were issues in either obtaining the {@link ProtectionElement} or the {@link User} or in the
+	 * actual assignment of ownership
 	 */
 	public void setOwnerForProtectionElement(String userName, String protectionElementObjectId, String protectionElementAttributeName)throws CSTransactionException;
 
@@ -228,16 +240,16 @@ public interface AuthorizationManager {
 	public void initialize(String applicationContextName);
 
 	/**
-	 * This methods return a List of all the protection groups
-	 * @return List 
+	 * This methods return a List of all the {@link ProtectionGroup} for the current application.
+	 * @return List List of all the Protection Groups for the current application
 	 */
 	public java.util.List getProtectionGroups();
 	
 	/**
-	 * This method returns the ProtectionElement for a given objectId and attributeName
-	 * @param objectId
-	 * @param attributeName
-	 * @return ProtectionElement
+	 * This method returns the {@link ProtectionElement} for a given objectId and attributeName
+	 * @param objectId The object id of the protection element to be obtained
+	 * @param attributeName The attribute name of the protection element to be obtained
+	 * @return ProtectionElement Returns the {@link ProtectionElement} if found else null
 	 */
 	public ProtectionElement getProtectionElement(String objectId,String attributeName);
 	
@@ -246,19 +258,22 @@ public interface AuthorizationManager {
 	 *  This method assumes that Object passed has all the public getter and setter methods. The method checks
 	 *  permission for every attribute of the object for the passed userName. If the user does not have read
 	 *  permission on the few attributes, then they will appear as null.
-	 * @param userName
-	 * @param obj
-	 * @return Object
-	 * @throws CSException
+	 * @param userName The user name for the User who is trying to access the object
+	 * @param obj The Java Bean data object whose attribute needs to be protected.
+	 * @return Object The mutated Java Bean object which is a copy of the original object. This object doesnt not contain
+	 * values for the attributes on which the user doesnot have permission
+	 * @throws CSException If there is any problem the checking for access permissions for securing the object
 	 */
 	public Object secureObject(String userName, Object obj) throws CSException;
 	
 	/**
-	 * The secureCollection method secures a collection of objects which follow java beans specifications.
-	 * @param userName
-	 * @param objects
-	 * @return Collection
-	 * @throws CSException
+	 * This methods works the same way as the {@link secureObject} except that it accepts an collection of Objects
+	 * rather then single instance
+	 * @param userName The user name for the User who is trying to access the object
+	 * @param objects The collection Java Bean data objects whose attribute needs to be protected.
+	 * @return Collection The collection mutated Java Bean objects which are a copy of the original objects. These objects doesnt not contain
+	 * values for the attributes on which the user doesnot have permission.
+	 * @throws CSException If there is any problem the checking for access permissions for securing the objects
 	 */
 	public Collection secureCollection(String userName,Collection objects) throws CSException;
 	
@@ -272,19 +287,39 @@ public interface AuthorizationManager {
 	public Set getProtectionGroups(String protectionElementId) throws CSObjectNotFoundException;
 	
 	/**
-	 * The method return a Collection of <code>ObjectPrivilegeMap</code>. For every passed <code>ProtectionElement</code>,the 
-	 * method looks for the privileges that this user have on the <code>ProtectionElement</code>
-	 * @param userName
-	 * @param protectionElements
-	 * @return Collection
-	 * @throws CSException
+	 * The method returns a Collection of {@link ObjectPrivilegeMap}. For every passed <code>ProtectionElement</code>,the 
+	 * method looks for the privileges that this {@link User} have on the {@link ProtectionElement}
+	 * @param userName The user name for the User who is trying to access the object
+	 * @param protectionElements Collection of Protection Elements for which the access priveleges are to be retrieved for the given user
+	 * @return Collection The collection of {@link ObjectPrivilegeMap}
+	 * @throws CSException If there is any problem obtaining the access permissions for securing the objects
 	 */
 	public Collection getPrivilegeMap(String userName,Collection protectionElements) throws CSException;
 	
+	
+	/**
+	 * This method accepts the original as well as the mutated object. It then retrieved the Attribute level privilege using
+	 * the object's name. Using this privilege access map it checks if the attributes on which the {@link User} doesnt have 
+	 * access has been modified in the mutated object. If found then it replaces the value of the same back from the original
+	 * object. Thus this methods doesnt allow modification of attributes of an object on which the user doesnt have access.
+	 * This new object is returned which contains the changed values only for those attributes on which the User has access.
+	 * @param userName The user name of the {@link User} which is trying to update the object
+	 * @param originalObject The original data object as it was read from the data base
+	 * @param mutatedObject The data object which contains the changes which the user has made
+	 * @return The object which contains the changed values only for those attributes on which the User has access.
+	 * @throws CSException If there is any problem obtaining the access permissions for securing the objects
+	 */
 	public Object secureUpdate(String userName, Object originalObject,Object mutatedObject) throws CSException;
 	
-	public boolean checkOwnership(String userName,
-			String protectionElementObjectId);
+	/**
+	 * The methods checks if the given {@link User} is owner of the {@link ProtectionElement}. The {@link  User} object is
+	 * obtained using the user name provided . The {@link ProtectionElement} is retrieved using the object id passed. Even though
+	 * if the user has ownership on a particular attribute of the object then he has ownership on the entire object
+	 * @param userName The user for which the ownership for the protection element is to be determined
+	 * @param protectionElementObjectId The object id of the protection element for which the ownership is to be determined 
+	 * @return True if the user has ownership on the protection element else False
+	 */
+	public boolean checkOwnership(String userName, String protectionElementObjectId);
 	
 }
 
