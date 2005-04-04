de2267d31f34373de5bc5b5132210b8cb18d0207
hustedb <hustedb>
2005-04-04 00:58:52 +0000

    refactored
    SVN-Revision: 854

--- ri/src/gov/nih/nci/security/ri/dao/SecurityRIDAO.java
+++ ri/src/gov/nih/nci/security/ri/dao/SecurityRIDAO.java
@@ -2,6 +2,8 @@ package gov.nih.nci.security.ri.dao;
 
 import java.net.URL;
 
+import javax.naming.ConfigurationException;
+
 import net.sf.hibernate.HibernateException;
 import net.sf.hibernate.Session;
 import net.sf.hibernate.SessionFactory;
@@ -12,6 +14,7 @@ import org.apache.log4j.Logger;
 
 /**
  * Base class for DAO Persistence and Retreival
+ * 
  * @author Brian Husted
  *  
  */
@@ -20,18 +23,6 @@ public class SecurityRIDAO {
 	private static SessionFactory sessionFactory = null;
 
 	static final Logger log = Logger.getLogger(SecurityRIDAO.class.getName());
-
-	static {
-		try {
-			URL url = SecurityRIDAO.class.getClassLoader().getResource(
-					"hibernate.cfg.xml");
-			log.debug( "The url to the config file is: " + url.toString() );
-			setSessionFactory(new Configuration().configure(url)
-					.buildSessionFactory());
-		} catch (Exception ex) {
-			log.fatal("Unable to create SessionFactory", ex);
-		}
-	}
 
 	protected static void saveObject(Object o) throws HibernateException {
 		Session s = null;
@@ -81,9 +72,9 @@ public class SecurityRIDAO {
 		}
 	}
 
-	
 	/**
 	 * Returns a value object based on the class type and primary key.
+	 * 
 	 * @param c
 	 * @param primaryKey
 	 * @return
@@ -112,14 +103,27 @@ public class SecurityRIDAO {
 	 * @return Returns the sessionFactory.
 	 */
 	protected static SessionFactory getSessionFactory() {
+		if (sessionFactory != null) {
+
+			synchronized (SecurityRIDAO.class) {
+				if (sessionFactory != null) {
+
+					try {
+						URL url = SecurityRIDAO.class.getClassLoader()
+								.getResource("hibernate.cfg.xml");
+						log.debug("The url to the config file is: "
+								+ url.toString());
+						sessionFactory = new Configuration().configure(url)
+								.buildSessionFactory();
+					} catch (Exception ex) {
+						log.fatal("Unable to create SessionFactory", ex);
+						throw new RuntimeException(
+								"Unable to create SessionFactory", ex);
+					}
+				}
+			}
+		}
 		return sessionFactory;
 	}
 
-	/**
-	 * @param sessionFactory
-	 *            The sessionFactory to set.
-	 */
-	public static void setSessionFactory(SessionFactory sessionFactory) {
-		SecurityRIDAO.sessionFactory = sessionFactory;
-	}
 }
