13dffaaab7a13cd34bf79ae4815b69131ba1cd33
Vinay Kumar <vinay.kumar@nih.gov>
2005-06-03 15:57:12 +0000

    *** empty log message ***
    SVN-Revision: 1114

---
+++ src/gov/nih/nci/sdk/codegen/remoting/RemoteServiceGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jun 3, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.sdk.codegen.remoting;
+
+/**
+ * @author kumarvi
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class RemoteServiceGenerator {
+
+	public static void main(String[] args) {
+	}
+}
