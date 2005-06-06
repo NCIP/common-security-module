b2f3d1fe4d31c761cea13b5d0606f7135ef50948
Vinay Kumar <vinay.kumar@nih.gov>
2005-06-06 15:01:51 +0000

    *** empty log message ***
    SVN-Revision: 1119

--- src/gov/nih/nci/sdk/codegen/clientlayer/ApplicationServiceClientImpl_Creator.java
+++ src/gov/nih/nci/sdk/codegen/clientlayer/ApplicationServiceClientImpl_Creator.java
@@ -11,8 +11,28 @@ package gov.nih.nci.sdk.codegen.clientlayer;
  *
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * This class will generate the ApplicationServiceClientImpl, which implemenets 
+ * ApplicationService interface and has dependency on RemoteApplicationService
  */
 public class ApplicationServiceClientImpl_Creator {
+	
+	public Class applicationServiceInterface;
+	
+	public ApplicationServiceClientImpl_Creator(Class applicationServiceInterface){
+		this.applicationServiceInterface= applicationServiceInterface;
+	}
+	
+	public void generate(){
+		
+	}
+	private void createXMLGlue_forRemoteService_Injection(){
+		
+	}
+	private void implementMethods(){
+		
+	}
+	
 
 	public static void main(String[] args) {
 	}
