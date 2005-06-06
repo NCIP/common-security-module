e0203508ec7c4792be459a4fb393478f91e0b140
Vinay Kumar <vinay.kumar@nih.gov>
2005-06-06 15:10:50 +0000

    *** empty log message ***
    SVN-Revision: 1120

---
+++ src/com/ekagrasoft/server/management/CSMEnabler.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jun 6, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.ekagrasoft.server.management;
+
+/**
+ * @author kumarvi
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CSMEnabler {
+
+	public static void main(String[] args) {
+	}
+}
