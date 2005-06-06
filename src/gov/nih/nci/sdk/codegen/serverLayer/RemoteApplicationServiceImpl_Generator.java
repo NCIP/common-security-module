f584f2bedf2120662636eb878f76938560d675b3
Vinay Kumar <vinay.kumar@nih.gov>
2005-06-06 15:56:38 +0000

    *** empty log message ***
    SVN-Revision: 1121

---
+++ src/gov/nih/nci/sdk/codegen/serverLayer/RemoteApplicationServiceImpl_Generator.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Jun 6, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package gov.nih.nci.sdk.codegen.serverLayer;
+
+/**
+ * @author kumarvi
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ * 
+ * The generated class (RemoteApplicationServiceImpl) will implement RemoteApplicationService
+ */
+public class RemoteApplicationServiceImpl_Generator {
+
+	private Class applicationService;
+	
+	public RemoteApplicationServiceImpl_Generator(Class applicationServiceInterface){
+		this.applicationService= applicationServiceInterface;
+	}
+	
+	public static void main(String[] args) {
+	}
+}
