a72342f13feff1c4831af9faf94b2eb2a92c106f
Kunal Modi <modik@mail.nih.gov>
2006-01-26 18:57:41 +0000

    Made changes to add a logout call inorder to log user log outs.
    SVN-Revision: 1600

--- upt/webapp/src/gov/nih/nci/security/upt/actions/LogoutAction.java
+++ upt/webapp/src/gov/nih/nci/security/upt/actions/LogoutAction.java
@@ -95,6 +95,11 @@ package gov.nih.nci.security.upt.actions;
  */
 
 
+import java.io.File;
+
+import gov.nih.nci.logging.api.user.UserInfoHelper;
+import gov.nih.nci.security.AuthenticationManager;
+import gov.nih.nci.security.SecurityServiceProvider;
 import gov.nih.nci.security.upt.constants.DisplayConstants;
 import gov.nih.nci.security.upt.constants.ForwardConstants;
 import gov.nih.nci.security.upt.forms.LoginForm;
@@ -108,6 +113,9 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
 
 /**
  * @author Kunal Modi (Ekagra Software Technologies Ltd.)
@@ -120,14 +128,31 @@ public class LogoutAction extends Action
 	
 	private static final Logger log = Logger.getLogger(LogoutAction.class);
 	
+	
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
 	{
 		/* perform login task*/
 		HttpSession session = request.getSession();
+		String uptContextName = "";
+
+		UserInfoHelper.setUserInfo(((LoginForm)session.getAttribute(DisplayConstants.LOGIN_OBJECT)).getLoginId(), session.getId());
+		
+		try
+		{
+			uptContextName = getUPTContextName();
+			AuthenticationManager authenticationManager = SecurityServiceProvider.getAuthenticationManager(uptContextName);
+			authenticationManager.logout(((LoginForm)session.getAttribute(DisplayConstants.LOGIN_OBJECT)).getLoginId());
+		}
+		catch (Exception e)
+		{
+			if (log.isDebugEnabled())
+				log.debug(session.getId()+"|"+((LoginForm)session.getAttribute(DisplayConstants.LOGIN_OBJECT)).getLoginId()+
+						"||Logout|Failure|Logout Called and Error occured||"+ e.getMessage());		
+			e.printStackTrace();
+		}
 		if (log.isDebugEnabled())
 			log.debug(session.getId()+"|"+((LoginForm)session.getAttribute(DisplayConstants.LOGIN_OBJECT)).getLoginId()+
 					"||Logout|Success|Logout Called and Forwarding to the Login Page||");		
-
 		session.removeAttribute(DisplayConstants.USER_PROVISIONING_MANAGER);
 		session.removeAttribute(DisplayConstants.LOGIN_OBJECT);
 		session.removeAttribute(DisplayConstants.CURRENT_TABLE_ID);
@@ -139,5 +164,22 @@ public class LogoutAction extends Action
 		
 		return (mapping.findForward(ForwardConstants.LOGOUT_SUCCESS));
 	}
+	
+	private static String getUPTContextName() throws Exception
+	{
+		Document configDocument = null;
+		String uptContextNameValue = null;
+		String configFilePath = System.getProperty(DisplayConstants.CONFIG_FILE_PATH_PROPERTY_NAME);
+		SAXBuilder builder = new SAXBuilder();
+		configDocument = builder.build(new File(configFilePath));
+		if (configDocument != null)
+		{
+			Element securityConfig = configDocument.getRootElement();
+			Element uptContextName = securityConfig.getChild("upt-context-name");
+			uptContextNameValue = uptContextName.getText().trim();
+		}
+		return uptContextNameValue;
+	}
+
 
 }
