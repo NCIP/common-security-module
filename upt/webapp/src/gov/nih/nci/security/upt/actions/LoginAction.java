fa8e0e5143cf9f9439e75adacc8446fbce7779ab
Vijay Parmar <parmarv@mail.nih.gov>
2006-11-10 16:21:49 +0000

    *** empty log message ***
    SVN-Revision: 2103

--- upt/webapp/src/gov/nih/nci/security/upt/actions/LoginAction.java
+++ upt/webapp/src/gov/nih/nci/security/upt/actions/LoginAction.java
@@ -1,9 +1,3 @@
-/*
- * Created on Dec 20, 2004
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 package gov.nih.nci.security.upt.actions;
 
 /**
