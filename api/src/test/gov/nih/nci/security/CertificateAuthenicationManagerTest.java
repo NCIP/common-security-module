53fad936ad4ec756b2fd737ce1bb62f968904082
Vijay Parmar <parmarv@mail.nih.gov>
2007-04-20 16:55:15 +0000

    GForge ID #4562
    SVN-Revision: 2317

---
+++ api/src/test/gov/nih/nci/security/CertificateAuthenicationManagerTest.java
@@ -0,0 +1,150 @@
+package test.gov.nih.nci.security;
+
+import gov.nih.nci.security.AuthenticationManager;
+import gov.nih.nci.security.SecurityServiceProvider;
+import gov.nih.nci.security.authentication.principal.LoginIdPrincipal;
+import gov.nih.nci.security.exceptions.CSException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Properties;
+
+import javax.security.auth.Subject;
+
+import junit.framework.TestCase;
+
+public class CertificateAuthenicationManagerTest extends TestCase {
+	
+	Properties props = null;
+	private static AuthenticationManager authenticationManagerX509= null;
+
+	public CertificateAuthenicationManagerTest(String arg0) {
+		super(arg0);
+	}
+
+	protected void setUp() throws Exception {
+		URL url = null;
+		String path = null;
+		super.setUp();
+		
+		props = System.getProperties();
+		url = this.getClass().getClassLoader().getSystemResource("ApplicationSecurityConfig.xml");
+		path = url.getPath();
+		props.setProperty("gov.nih.nci.security.configFile", path.substring(1,(path.length())));
+		url = this.getClass().getClassLoader().getSystemResource("login.config");
+		path = url.getPath();		
+		props.setProperty("java.security.auth.login.config", path);
+		authenticationManagerX509 = getauthenticationManagerX509();
+	}
+	
+	private AuthenticationManager getauthenticationManagerX509(){
+		if (authenticationManagerX509 == null )
+		{
+			try
+			{
+				authenticationManagerX509 = SecurityServiceProvider.getAuthenticationManager("X509");
+			}
+			catch (CSException e)
+			{
+				fail();
+			}
+		}
+		return authenticationManagerX509;
+	}
+	
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	
+	
+		
+	public void testLoginX509_SubjectNull() {
+
+		boolean isValid = false;		
+		try
+		{
+			Subject subject = null;	
+			isValid = !(authenticationManagerX509.authenticate(subject));
+		}
+		catch(CSException cse)
+		{
+			isValid = true;
+		}
+		assertEquals(true, isValid);
+	}
+
+	public void testLoginX509_SubjectWithLoginIdPrincipal() {
+
+		boolean isValid = false;		
+		try
+		{
+			Subject subject = getSubjectWithLoginIdPrincipal();
+			isValid = authenticationManagerX509.authenticate(subject);
+		}
+		catch(CSException cse)
+		{
+			isValid = false;
+		}
+		assertEquals(true, isValid);
+	}
+	
+	/**
+	 * It assumes that a certificate with alias 'caxchange' is already imported 
+	 * and available in the keystore specified in the X509 Login Module configuration
+	 */
+	public void testLoginX509_SubjectWithCertificate() {
+
+		boolean isValid = false;		
+		try
+		{
+			Subject subject = getSubjectWithLoginIdPrincipal();
+			isValid = authenticationManagerX509.authenticate(subject);
+		}
+		catch(CSException cse)
+		{
+			isValid = false;
+		}
+		assertEquals(true, isValid);
+	}
+
+	private Subject getSubjectWithLoginIdPrincipal() {
+		Subject s = new Subject();
+		s.getPrincipals().add(new LoginIdPrincipal("caxchange"));
+		return s;
+	}
+
+	private Subject getSubjectWithCertificate() throws FileNotFoundException, CertificateException{
+		Subject s = new Subject();
+		
+		String certPath = "C://workspace//csm_api//src//parmarvcertfile.cer";
+		
+		CertificateFactory certificatefactory = CertificateFactory.getInstance("X.509");
+	
+		// Open the file.
+		FileInputStream fileinputstream = new FileInputStream(
+				certPath);
+		
+		// Generate a certificate from the data in the file.
+		X509Certificate x509certificate = (X509Certificate) certificatefactory
+				.generateCertificate(fileinputstream);
+		
+		
+		s.getPublicCredentials().add(x509certificate);
+			
+		return s;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ api/src/test/gov/nih/nci/security/authentication/CerficateReader.java
@@ -0,0 +1,68 @@
+package test.gov.nih.nci.security.authentication;
+
+import java.util.Set;
+import java.util.Iterator;
+import java.io.FileInputStream;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+public class CerficateReader {
+	public static void main(String[] arstring) {
+		try {
+			// Get the correct certificate factory.
+			CertificateFactory certificatefactory = CertificateFactory
+					.getInstance("X.509");
+			// Each file specified on the command line must contain a single
+			// DER-encoded X.509 certificate. The DER-encoded certificate
+			// can be in either binary or ASCII format.
+			for (int i = 0; i < arstring.length; i++) {
+				// Open the file.
+				FileInputStream fileinputstream = new FileInputStream(
+						arstring[i]);
+				System.out.println(arstring[i]);
+				// Generate a certificate from the data in the file.
+				X509Certificate x509certificate = (X509Certificate) certificatefactory
+						.generateCertificate(fileinputstream);
+				// First, let's print out information about the certificate
+				// itself.
+				System.out.println("---Certificate---");
+				System.out.println("type = " + x509certificate.getType());
+				System.out.println("version = " + x509certificate.getVersion());
+				System.out.println("subject = "
+						+ x509certificate.getSubjectDN().getName());
+				System.out.println("valid from = "
+						+ x509certificate.getNotBefore());
+				System.out.println("valid to = "
+						+ x509certificate.getNotAfter());
+				System.out.println("serial number = "
+						+ x509certificate.getSerialNumber().toString(16));
+				System.out.println("issuer = "
+						+ x509certificate.getIssuerDN().getName());
+				System.out.println("signing algorithm = "
+						+ x509certificate.getSigAlgName());
+				System.out.println("public key algorithm = "
+						+ x509certificate.getPublicKey().getAlgorithm());
+				// Next, let's print out information about the extensions.
+				System.out.println("---Extensions---");
+				Set setCritical = x509certificate.getCriticalExtensionOIDs();
+				if (setCritical != null && setCritical.isEmpty() == false)
+					for (Iterator iterator = setCritical.iterator(); iterator
+							.hasNext();)
+						System.out.println(iterator.next().toString()
+								+ " *critical*");
+				Set setNonCritical = x509certificate
+						.getNonCriticalExtensionOIDs();
+				if (setNonCritical != null && setNonCritical.isEmpty() == false)
+					for (Iterator iterator = setNonCritical.iterator(); iterator
+							.hasNext();)
+						System.out.println(iterator.next().toString());
+				// We're done.
+				System.out.println("---");
+				// Close the file.
+				fileinputstream.close();
+			}
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+}
