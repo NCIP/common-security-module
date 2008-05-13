c9d74e5aac909a41fc192230647a2290afe8a832
Vijay Parmar <parmarv@mail.nih.gov>
2008-05-13 14:42:41 +0000

    added getFiltersForUser() and getFiltersForGroups() method.
    SVN-Revision: 2530

--- api/src/gov/nih/nci/security/authorization/instancelevel/InstanceLevelSecurityHelper.java
+++ api/src/gov/nih/nci/security/authorization/instancelevel/InstanceLevelSecurityHelper.java
@@ -1,12 +1,11 @@
 package gov.nih.nci.security.authorization.instancelevel;
 
 import gov.nih.nci.security.AuthorizationManager;
-import gov.nih.nci.security.authorization.domainobjects.Application;
 import gov.nih.nci.security.authorization.domainobjects.FilterClause;
-import gov.nih.nci.security.dao.ApplicationSearchCriteria;
 import gov.nih.nci.security.dao.FilterClauseSearchCriteria;
 import gov.nih.nci.security.dao.SearchCriteria;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -59,6 +58,31 @@ public class InstanceLevelSecurityHelper
 		}
 	}
 	
+	public static List<FilterDefinition> getFiltersForGroups(AuthorizationManager authorizationManager)
+	{
+		List<FilterDefinition> filterDefinitionList = new ArrayList<FilterDefinition>();
+		FilterClause searchFilterClause = new FilterClause();
+		searchFilterClause.setClassName("*");
+		SearchCriteria searchCriteria = new FilterClauseSearchCriteria(searchFilterClause);
+		List list = authorizationManager.getObjects(searchCriteria);
+		Iterator iterator = list.iterator();
+		while (iterator.hasNext())
+		{
+			HashMap parameters = new HashMap();
+			parameters.put("GROUP_NAMES", new StringType());
+			parameters.put("APPLICATION_ID", new LongType());
+
+			FilterClause filterClause = (FilterClause)iterator.next();
+			FilterDefinition filterDefinition = new FilterDefinition (
+					filterClause.getClassName().substring(
+							filterClause.getClassName().lastIndexOf('.') + 1) + filterClause.getId(),
+							filterClause.getGeneratedSQLForGroup(), 
+							parameters);
+			if(filterDefinition!=null) filterDefinitionList.add(filterDefinition);
+		}
+		return filterDefinitionList;
+	}
+	
 	/**
 	 * This method injects the security filters which are created for this application. It retrieves a list of all the filters which have 
 	 * been defined for this application from the CSM Database. Now for each filter in the list, it creates a new FilterDefinition object.
@@ -92,7 +116,46 @@ public class InstanceLevelSecurityHelper
 		
 	}
 
+	
+	/**
+	 * This method injects the security filters which are created for this application. It retrieves a list of all the filters which have 
+	 * been defined for this application from the CSM Database. Now for each filter in the list, it creates a new FilterDefinition object.
+	 * It then retrieves the Persistent Class from the passed Configuration Object using the class name for which the filter is defined.
+	 * It then adds the filter to the persistent class by setting the filtering query.
+	 * @param authorizationManager The CSM AuthorizationManager instance for this application
+	 * @param configuration The Hibernate Configuration initialized for this application
+	 */
+	public static List<FilterDefinition> getFiltersForUser(AuthorizationManager authorizationManager)
+	{
 
+		List<FilterDefinition> filterDefinitionList = new ArrayList<FilterDefinition>();
+		
+		FilterClause searchFilterClause = new FilterClause();
+		searchFilterClause.setClassName("*");
+		SearchCriteria searchCriteria = new FilterClauseSearchCriteria(searchFilterClause);
+		List list = authorizationManager.getObjects(searchCriteria);
+		Iterator iterator = list.iterator();
+		while (iterator.hasNext())
+		{
+			HashMap parameters = new HashMap();
+			parameters.put("USER_NAME", new StringType());
+			parameters.put("APPLICATION_ID", new LongType());
+
+			FilterClause filterClause = (FilterClause)iterator.next();
+			FilterDefinition filterDefinition = new FilterDefinition (
+					filterClause.getClassName().substring(filterClause.getClassName().lastIndexOf('.') + 1) + filterClause.getId(),  
+					filterClause.getGeneratedSQLForUser(),
+					parameters);
+			if(filterDefinition!=null) filterDefinitionList.add(filterDefinition);
+		}
+		return filterDefinitionList;
+		
+		
+	}
+
+
+	
+	
 	/**
 	 * This method initializes the filter that are already added to the Sessionfactory. This method first obtains the list of all the 
 	 * defined filters from the SessionFactory in the passes Session object. It then just iterates through the filter list and sets 
