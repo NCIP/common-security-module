763b9dcb4c3ef68fba95b16d24f50a9c6edd6cea
Vijay Parmar <parmarv@mail.nih.gov>
2008-09-29 07:31:36 +0000

    improved exception handling.
    SVN-Revision: 2761

--- cgmmweb/src/gov/nih/nci/security/cgmm/helper/AuthenticationServiceHelper.java
+++ cgmmweb/src/gov/nih/nci/security/cgmm/helper/AuthenticationServiceHelper.java
@@ -1,12 +1,13 @@
 package gov.nih.nci.security.cgmm.helper;
 
 import gov.nih.nci.cagrid.opensaml.SAMLAssertion;
+import gov.nih.nci.security.cgmm.exceptions.CGMMAuthenticationURLException;
 import gov.nih.nci.security.cgmm.exceptions.CGMMGridAuthenticationServiceException;
 import gov.nih.nci.security.cgmm.exceptions.CGMMInputException;
 
 public interface AuthenticationServiceHelper
 {
 
-	public SAMLAssertion authenticate(String authenticationServiceURL, String userName, String password) throws  CGMMInputException, CGMMGridAuthenticationServiceException;
+	public SAMLAssertion authenticate(String authenticationServiceURL, String userName, String password) throws  CGMMInputException, CGMMGridAuthenticationServiceException, CGMMAuthenticationURLException;
 
 }
