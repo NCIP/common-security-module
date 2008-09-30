bab832f636195391feacf8c5e87ba4e6b21f6419
Vijay Parmar <parmarv@mail.nih.gov>
2008-09-30 08:21:57 +0000

    SVN-Revision: 2791

--- software/cgmmweb/src/test/CGMMManagerTest.java
+++ software/cgmmweb/src/test/CGMMManagerTest.java
@@ -1,10 +1,39 @@
 package test;
 
+import gov.nih.nci.security.cgmm.CGMMManager;
+import gov.nih.nci.security.cgmm.CGMMManagerImpl;
+import gov.nih.nci.security.cgmm.exceptions.CGMMConfigurationException;
+
+import java.util.SortedMap;
+
 public class CGMMManagerTest {
 
 	public static void main(String[] args) {
-		
+		System.setProperty("gov.nih.nci.security.cgmm.syncgts.file","C:/Vijay/software/jboss-4.0.5.GA/server/default/conf/sync-description.xml");
+		System.setProperty("gov.nih.nci.security.cgmm.properties.file","C:/Vijay/software/jboss-4.0.5.GA/server/default/conf/cgmm-properties.xml");
+		System.setProperty("gov.nih.nci.security.cgmm.login.config.file","C:/Vijay/software/jboss-4.0.5.GA/server/default/conf/cgmm.login.config");
 
+
+//		Get AuthenticationService URL Info.
+		CGMMManager cgmmManager=null;
+		try {
+			cgmmManager = new CGMMManagerImpl();
+		} catch (CGMMConfigurationException e) {
+			e.printStackTrace();
+		}
+		
+		SortedMap authenticationServiceURLMap =null;
+		try {
+			 authenticationServiceURLMap =  cgmmManager.getAuthenticationServiceURLMap();
+			 if(authenticationServiceURLMap==null) System.out.println(" Map is null");
+			 else {
+				 System.out.println(" Got Map");
+			 }
+				 
+		} catch (CGMMConfigurationException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
