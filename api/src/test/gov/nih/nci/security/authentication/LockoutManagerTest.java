7e010268cc52e5a2e39f46dc66a6f62cb0323678
Santosh garmilla <garmillas@mail.nih.gov>
2008-10-15 19:42:26 +0000

    test case for lockout manager
    SVN-Revision: 2826

---
+++ api/src/test/gov/nih/nci/security/authentication/LockoutManagerTest.java
@@ -0,0 +1,39 @@
+package test.gov.nih.nci.security.authentication;
+
+import gov.nih.nci.security.authentication.LockoutManager;
+import junit.framework.TestCase;
+
+public class LockoutManagerTest extends TestCase{
+	
+	private int value=0;
+	public void testLockManager() throws Exception{
+		
+		for(int i=0;i<200;i++){
+			String string = " "+i+" ";
+			ThreadExample example=new ThreadExample(string);
+			example.start();
+			LockoutManager lockoutManager=LockoutManager.getInstance();
+			lockoutManager.unLockUser(""+value);
+			Thread.sleep(1);
+		}
+	}
+	
+	private class ThreadExample extends Thread{
+		
+		public ThreadExample(String threadName) {
+			super(threadName);
+		}
+		@Override
+		public synchronized void start() {
+			super.start();
+			System.out.println("Start method called "+this.getName());
+		}
+		@Override
+		public void run() {
+			super.run();
+			System.out.println("Run method called "+this.getName());
+			LockoutManager lockoutManager =LockoutManager.getInstance();
+			lockoutManager.setFailedAttempt(""+value++);
+		}
+	}
+}
