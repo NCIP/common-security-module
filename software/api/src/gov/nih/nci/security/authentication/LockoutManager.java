4fd58a18de72e706b8f4439a6350f8c3f3bc07e3
Santosh garmilla <garmillas@mail.nih.gov>
2008-10-15 19:39:31 +0000

    added mutual exclusion lock condition for object lockoutCache
    SVN-Revision: 2825

--- software/api/src/gov/nih/nci/security/authentication/LockoutManager.java
+++ software/api/src/gov/nih/nci/security/authentication/LockoutManager.java
@@ -8,107 +8,59 @@ import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class LockoutManager
-{
+public class LockoutManager {
 
-	private static Hashtable lockoutCache = null;
+	private static Hashtable<String,LockoutInfo> lockoutCache = null;
 	private static LockoutManager lockoutManager = null;
-	static Timer cleanupTimer = new Timer();
+	private static Timer cleanupTimer = new Timer();
 
 	private long lockoutTime = 1800000;
 	private long allowedLoginTime;
 	private int allowedAttempts;
 	private boolean disableLockoutManager;
 	private long delayTime;
+	private Object mutex;
 	
-	private class CleanupTask extends TimerTask
-	{
-		public void run()
-		{
-			//Collection<String> userIds = lockoutCache.keySet(); - Removed to support java1.4 osr
-			Collection userIds = (Collection)lockoutCache.keySet();
-			Iterator iter = userIds.iterator();
-			//for (String userId : userIds) - Removed to support java1.4 osr
-			while (iter.hasNext())
-			{
-				String userId = (String) iter.next();
-				LockoutInfo lockoutInfo = (LockoutInfo)lockoutCache.get(userId);
-				if (delayTime < (System.currentTimeMillis() - lockoutInfo.getFirstLoginTime()))
-				{
-					lockoutCache.remove(userId);
+	private class CleanupTask extends TimerTask {
+		public void run() {
+			synchronized (mutex) {
+				Collection<String> userIds = (Collection<String>) lockoutCache.keySet();
+				Iterator iter = userIds.iterator();
+				while (iter.hasNext()) {
+					String userId = (String) iter.next();
+					LockoutInfo lockoutInfo = (LockoutInfo) lockoutCache.get(userId);
+					if (delayTime < (System.currentTimeMillis() - lockoutInfo.getFirstLoginTime())) {
+						lockoutCache.remove(userId);
+					}
 				}
 			}
 		}
 	}
 
-	protected class LockoutInfo
-	{
-		private int noOfAttempts;
-		private long firstLoginTime;
-		private boolean lockedout;
-
-		public long getFirstLoginTime()
-		{
-			return firstLoginTime;
-		}
-
-		public void setFirstLoginTime(long firstLoginTime)
-		{
-			this.firstLoginTime = firstLoginTime;
-		}
-
-		public int getNoOfAttempts()
-		{
-			return noOfAttempts;
-		}
-
-		public void setNoOfAttempts(int noOfAttempts)
-		{
-			this.noOfAttempts = noOfAttempts;
-		}
-
-		public boolean isLockedout()
-		{
-			return lockedout;
-		}
-
-		public void setLockedout(boolean lockedout)
-		{
-			this.lockedout = lockedout;
-		}
-		
-	}
-
-	private LockoutManager(String lockoutTime, String allowedLoginTime, String allowedAttempts)
-	{
+	private LockoutManager(String lockoutTime, String allowedLoginTime,String allowedAttempts) {
 		lockoutManager = new LockoutManager();
-		//lockoutCache = new HashMap();
-		if (lockoutTime.equals("0") || allowedLoginTime.equals("0") || allowedAttempts.equals("0"))
+		mutex=new Object();
+		if (lockoutTime.equals("0") || allowedLoginTime.equals("0")
+				|| allowedAttempts.equals("0"))
 			disableLockoutManager = true;
-		else
-		{
-			//this.lockoutTime = new Long(lockoutTime); - Removed to support java1.4 osr
+		else {
 			this.lockoutTime = new Long(lockoutTime).longValue();
-			//this.allowedLoginTime = new Long(allowedLoginTime); - Removed to support java1.4 osr
 			this.allowedLoginTime = new Long(allowedLoginTime).longValue();
 			this.allowedAttempts = Integer.parseInt(allowedAttempts);
 			this.disableLockoutManager = false;
 			this.delayTime = this.lockoutTime + this.allowedLoginTime;
 			cleanupTimer.schedule(new CleanupTask(), delayTime, delayTime);
 		}
-
 	}
 	
-	private LockoutManager()
-	{
-		lockoutCache = new Hashtable();
+	private LockoutManager() {
+		lockoutCache = new Hashtable<String, LockoutInfo>();
 	}
 	
-
-	public static void initialize(String lockoutTime, String allowedLoginTime, String allowedAttempts)
-	{
-		if(null==lockoutManager){
-				lockoutManager = new LockoutManager(lockoutTime, allowedLoginTime, allowedAttempts);
+	public static void initialize(String lockoutTime, String allowedLoginTime,
+			String allowedAttempts) {
+		if (null == lockoutManager) {
+			lockoutManager = new LockoutManager(lockoutTime, allowedLoginTime,allowedAttempts);
 		}
 	}
 	
@@ -117,59 +69,84 @@ public class LockoutManager
 		return lockoutManager;
 	}
 
-	public boolean isUserLockedOut(String userId)
-	{
-		if (!disableLockoutManager)
-		{
-			LockoutInfo lockoutInfo = (LockoutInfo)lockoutCache.get(userId);
-			if (null != lockoutInfo)
-				return lockoutInfo.isLockedout();
-			else
-				return false;
-		}
-		else
+	public boolean isUserLockedOut(String userId) {
+		if (!disableLockoutManager) {
+			synchronized (mutex) {
+				LockoutInfo lockoutInfo = (LockoutInfo) lockoutCache.get(userId);
+				if (null != lockoutInfo)
+					return lockoutInfo.isLockedout();
+				else
+					return false;
+			}
+		} else
 			return false;
 	}
 	
-	public boolean setFailedAttempt(String userId)
-	{
+	public boolean setFailedAttempt(String userId) {
 		boolean isUserLockedout = false;
-		if (!disableLockoutManager)
-		{
-			LockoutInfo lockoutInfo = (LockoutInfo)lockoutCache.get(userId);
-			if (null != lockoutInfo)
-			{
-				if (!lockoutInfo.isLockedout())
-				{
-					if ((System.currentTimeMillis() - lockoutInfo.getFirstLoginTime()) < allowedLoginTime)
-					{
-						lockoutInfo.setNoOfAttempts(lockoutInfo.getNoOfAttempts()+ 1);
-						if (lockoutInfo.getNoOfAttempts() >= allowedAttempts)
-						{
+		if (!disableLockoutManager) {
+			LockoutInfo lockoutInfo=null;
+			synchronized (mutex) {
+				lockoutInfo = (LockoutInfo) lockoutCache.get(userId);
+			}
+			if (null != lockoutInfo) {
+				if (!lockoutInfo.isLockedout()) {
+					if ((System.currentTimeMillis() - lockoutInfo.getFirstLoginTime()) < allowedLoginTime) {
+						lockoutInfo.setNoOfAttempts(lockoutInfo.getNoOfAttempts() + 1);
+						if (lockoutInfo.getNoOfAttempts() >= allowedAttempts) {
 							lockoutInfo.setLockedout(true);
 							isUserLockedout = true;
 						}
-					}
-					else
-					{
+					} else {
 						lockoutInfo.setFirstLoginTime(System.currentTimeMillis());
 						lockoutInfo.setNoOfAttempts(1);
-					}	
+					}
 				}
-			}
-			else
-			{
+			} else {
 				lockoutInfo = lockoutManager.new LockoutInfo();
 				lockoutInfo.setFirstLoginTime(System.currentTimeMillis());
-				lockoutInfo.setNoOfAttempts(1);			
+				lockoutInfo.setNoOfAttempts(1);
 			}
-			lockoutCache.put(userId,lockoutInfo);
+			synchronized (mutex) {
+				lockoutCache.put(userId, lockoutInfo);
+			}
 		}
 		return isUserLockedout;
 	}
+
+	public void unLockUser(String userId) {
+		synchronized (mutex) {
+			lockoutCache.remove(userId);
+		}
+	}
 	
-	public void unLockUser(String userId){
-		
-		lockoutCache.remove(userId);
+	protected class LockoutInfo {
+		private int noOfAttempts;
+		private long firstLoginTime;
+		private boolean lockedout;
+
+		public long getFirstLoginTime() {
+			return firstLoginTime;
+		}
+
+		public void setFirstLoginTime(long firstLoginTime) {
+			this.firstLoginTime = firstLoginTime;
+		}
+
+		public int getNoOfAttempts() {
+			return noOfAttempts;
+		}
+
+		public void setNoOfAttempts(int noOfAttempts) {
+			this.noOfAttempts = noOfAttempts;
+		}
+
+		public boolean isLockedout() {
+			return lockedout;
+		}
+
+		public void setLockedout(boolean lockedout) {
+			this.lockedout = lockedout;
+		}
 	}
 }
