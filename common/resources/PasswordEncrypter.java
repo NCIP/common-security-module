f0175873af04e93d0fc74c467c34b3307d743ca6
Vijay Parmar <parmarv@mail.nih.gov>
2009-04-29 12:42:40 +0000

    SVN-Revision: 3050

--- common/resources/PasswordEncrypter.java
+++ common/resources/PasswordEncrypter.java
@@ -8,7 +8,7 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.SQLException; 
 import java.sql.Statement;
 
 /**
