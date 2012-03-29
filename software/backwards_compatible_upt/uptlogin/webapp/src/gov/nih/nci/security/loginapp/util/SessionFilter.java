7f2e2d575a89bad87cd4f8a877f269781a9b37fc
Eugene Wang <wangeug@mail.nih.gov>
2012-03-29 18:52:44 +0000

    session cookie
    SVN-Revision: 3846

--- software/backwards_compatible_upt/uptlogin/webapp/src/gov/nih/nci/security/loginapp/util/SessionFilter.java
+++ software/backwards_compatible_upt/uptlogin/webapp/src/gov/nih/nci/security/loginapp/util/SessionFilter.java
@@ -8,29 +8,47 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class SessionFilter implements Filter {
-	private ArrayList<String> urlList;
+	private ArrayList<String> avoidUrlList;
 	public void destroy() {     }
 	public void doFilter(ServletRequest req, ServletResponse res,  FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
 		String url = request.getServletPath();
-		boolean allowedRequest = false;
 		System.out.println("gov.nih.nci.security.loginapp.util.SessionFilter.doFilter()..url:"+url);
-		if(urlList.contains(url)) {
-			allowedRequest = true;
+		HttpSession session =null;
+		if(avoidUrlList.contains(url)) {
 			//create  a new session if not exist
-			request.getSession();
+			session= request.getSession();
+			Cookie userCookie = new Cookie("sessionCookie", session.getId());
+			response.addCookie(userCookie);
 		}
-		if (!allowedRequest) {
-			HttpSession session = request.getSession(false);
-			if (null == session) {
-			response.sendRedirect("index.jsp");
-			}
+		else {
+			 session = request.getSession(false);
+				if (null == session) {
+					response.sendRedirect("index.jsp");
+					return;
+				}
+			 Cookie[] cookies=request.getCookies();
+			 String cookieValue="";
+			 for(int i=0; i<cookies.length; i++) 
+			 {
+			      Cookie cookie = cookies[i];
+			      if ("sessionCookie".equals(cookie.getName()))
+			    	  cookieValue=cookie.getValue();
+			 }
+			 System.out.println("SessionFilter.doFilter()...sessionCookie="+cookieValue);
+			 if (! cookieValue.equals(session.getId()))
+			 {
+				response.sendRedirect("index.jsp");
+				return;
+			 }
+
 		}
 		chain.doFilter(req, res);
 	}
@@ -40,9 +58,9 @@ public class SessionFilter implements Filter {
 		String urls = config.getInitParameter("avoid-urls");
 		System.out.println("gov.nih.nci.security.loginapp.util.SessionFilter.init()..avoid-urls:"+urls);
 		StringTokenizer token = new StringTokenizer(urls, ",");
-		urlList = new ArrayList<String>();
-		while (token.hasMoreTokens()) {
-		urlList.add(token.nextToken());
-		}
-		}
+		avoidUrlList = new ArrayList<String>();
+		while (token.hasMoreTokens()) 
+			avoidUrlList.add(token.nextToken());
+		
 	}
+}
