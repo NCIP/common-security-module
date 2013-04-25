1d83eb9dcba1bf14a0b52727c051211ce7bde23b
Prasad Konka <prasad.konka@nih.gov>
2013-04-24 22:44:54 -0400

    Adding copyright

--- software_backwards_compatibility/upt31/Distribution Zip File/UPTDataPriming.sql
+++ software_backwards_compatibility/upt31/Distribution Zip File/UPTDataPriming.sql
@@ -1,3 +1,11 @@
+/*L
+   Copyright Ekagra Software Technologies Ltd.
+   Copyright SAIC
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/common-security-module/LICENSE.txt for details.
+L*/
+
 insert into application(APPLICATION_NAME,APPLICATION_DESCRIPTION,DECLARATIVE_FLAG,ACTIVE_FLAG,UPDATE_DATE)
 values ("<<upt_context_name>>","UPT Super Admin Application",0,1,sysdate());
 
