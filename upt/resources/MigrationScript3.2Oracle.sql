04b821a754aecae5931ab5c5adeda8bace7cfa65
Prasad Konka <prasad.konka@nih.gov>
2013-04-24 22:52:01 -0400

    Adding copyright

--- upt/resources/MigrationScript3.2Oracle.sql
+++ upt/resources/MigrationScript3.2Oracle.sql
@@ -1,3 +1,11 @@
+/*L
+   Copyright Ekagra Software Technologies Ltd.
+   Copyright SAIC
+
+   Distributed under the OSI-approved BSD 3-Clause License.
+   See http://ncip.github.com/common-security-module/LICENSE.txt for details.
+L*/
+
 ALTER TABLE CSM_APPLICATION MODIFY  DECLARATIVE_FLAG NUMBER(1) DEFAULT 0 NOT NULL ;
 ALTER TABLE CSM_PROTECTION_ELEMENT DROP COLUMN PROTECTION_ELEMENT_TYPE_ID;
 ALTER TABLE CSM_PROTECTION_ELEMENT ADD PROTECTION_ELEMENT_TYPE VARCHAR(100);
