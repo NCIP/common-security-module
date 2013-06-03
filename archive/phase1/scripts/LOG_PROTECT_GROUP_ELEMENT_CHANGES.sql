5e5f8bf7b9e5751db6210c829563c5dba62b2bd7
Prasad Konka <prasad.konka@nih.gov>
2013-06-03 14:45:59 -0400

    Updated copyright header

--- archive/phase1/scripts/LOG_PROTECT_GROUP_ELEMENT_CHANGES.sql
+++ archive/phase1/scripts/LOG_PROTECT_GROUP_ELEMENT_CHANGES.sql
@@ -1,6 +1,6 @@
 /*L
    Copyright Ekagra Software Technologies Ltd.
-   Copyright SAIC
+   Copyright SAIC, SAIC-Frederick
 
    Distributed under the OSI-approved BSD 3-Clause License.
    See http://ncip.github.com/common-security-module/LICENSE.txt for details.
